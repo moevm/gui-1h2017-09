5231e79f903dce6a1d0e9493809c1016aa390e28
olkaprogrammist <its34play@yandex.ru>
2017-04-06 10:51:14 +0300

    First commt

---
+++ first.h
@@ -0,0 +1,17 @@
+#ifndef FIRST_H
+#define FIRST_H
+
+#include <QWidget>
+
+class First : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit First(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // FIRST_H
